7248f9d316e24a89a2ec1060340da2a7ac6766ee
20kdc <asdd2808@gmail.com>
2017-02-06 02:40:52 +0000

    [ONESHOT-SPECIFIC] Serialization may or may not work now (kind of untested, see desc.)
    Right now I'm running into an issue with the Scene stack acting funny around the messageboxes,
     specifically when the Entity decides to show up at the safe puzzle.
    This is a rather painful to reproduce situation because it requires running through 10 minutes of game just to get it.
    I'll be making sure to make message boxes appear in reverse order in the next commit or so,
     but I still need to figure out:
    
     1. why a question-type box causes this issue
     2. can this be recreated by using a question-type box where it shouldn't be,
        so testing can occur without so much time inbetween
     3. how to deal with the fact that Niko can't sleep thus breaking the attempt at handling the meta-element

--- src/oneshot_ser_ram.cpp
+++ src/oneshot_ser_ram.cpp
@@ -24,10 +24,21 @@
  * THE SOFTWARE.
  */
 
+#include <string.h>
 #include "oneshot.h"
 #include "oneshot_ser.h"
 
 static int oneshot_ser_ending = 0;
+
+static unsigned char oneshot_ser_hasSave = 0;
+
+static char oneshot_ser_username[256];
+static unsigned char oneshot_ser_usernameSize;
+static unsigned char oneshot_ser_items[256];
+static unsigned char * oneshot_ser_iptr;
+
+static uint8_t oneshot_ser_switches[200];
+static int32_t oneshot_ser_vars[100];
 
 int oneshot_ser_loadEnding() {
 	return oneshot_ser_ending;
@@ -38,6 +49,7 @@ void oneshot_ser_saveEnding(int ending) {
 }
 
 void oneshot_ser_wipeSave() {
+	oneshot_ser_hasSave = 0;
 }
 
 // Header Format: 1 byte for the size of the username.
@@ -45,13 +57,22 @@ void oneshot_ser_wipeSave() {
 // 200 bytes of switches.
 // 100 variables, each 4 bytes, little-endian.
 void oneshot_ser_saveBegin(uint8_t * switches, int32_t * vars, char * username, uint32_t usernameSize) {
-	// NYI
+	oneshot_ser_usernameSize = usernameSize;
+	memcpy(oneshot_ser_username, username, usernameSize);
+
+	oneshot_ser_iptr = oneshot_ser_items;
+
+	memcpy(oneshot_ser_switches, switches, 200);
+	memcpy(oneshot_ser_vars, vars, 100 * sizeof(int32_t));
 }
 // Item format is essentially a string - it's zero-terminated.
 // Writing is controlled mostly by the game script.
 // When a zero is written, the file is closed.
+// (Note: there are no safety checks on any of this)
 void oneshot_ser_saveItem(int item) {
-	// NYI
+	*(oneshot_ser_iptr++) = item;
+	if (item == 0)
+		oneshot_ser_hasSave = 1;
 }
 
 // 0: No save file exists.
@@ -60,12 +81,21 @@ void oneshot_ser_saveItem(int item) {
 // The calling function is responsible for updating the "oneshot" and "ending" internal variables,
 //  along with the cached on-quit message.
 int oneshot_ser_loadBegin(uint8_t * switches, int32_t * vars, char * username, uint32_t * usernameSize) {
-	// NYI
-	return 0;
+	if (!oneshot_ser_hasSave)
+		return 0;
+
+	*usernameSize = oneshot_ser_usernameSize;
+	memcpy(username, oneshot_ser_username, oneshot_ser_usernameSize);
+	username[oneshot_ser_usernameSize] = 0;
+
+	oneshot_ser_iptr = oneshot_ser_items;
+
+	memcpy(switches, oneshot_ser_switches, 200);
+	memcpy(vars, oneshot_ser_vars, 100 * sizeof(int32_t));
+	return 1;
 }
 int oneshot_ser_loadItem() {
-	// NYI
-	return 0;
+	return *(oneshot_ser_iptr++);
 }
 
 void oneshot_ser_document(char * string) {
