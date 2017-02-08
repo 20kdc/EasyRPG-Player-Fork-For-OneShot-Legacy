09c1b168152c390e16696dffaae9cfeeb6079bb3
20kdc <asdd2808@gmail.com>
2017-02-08 17:26:46 +0000

    [ONESHOT-SPECIFIC] Saving appears to work (?)
    Though where the files go is currently beyond me.

--- src/oneshot.h
+++ src/oneshot.h
@@ -2,6 +2,55 @@
 #ifndef _ONESHOT_H_
 #define _ONESHOT_H_
 
+// ---- CONFIGURATION
+
+// -- The following configure the oneshot_ser module:
+
+// Is <stdio.h> FILE* available? (If not, the saved game is held in RAM.)
+#define ONESHOT_DATA_PERSISTANCE
+
+// This is configuration for saving
+// The defaults right now are awful, but... better ideas, anyone?
+// (Under some circumstances this could be changed.)
+#ifdef WINDOWS
+#define ONESHOT_PRESAVE_COMMAND system("mkdir %appdata%/Oneshot");
+#define ONESHOT_WIPESAVE_COMMAND _unlink(buf);
+#define ONESHOT_SAVE_PATH "%s/Oneshot/save", getenv("APPDATA")
+#define ONESHOT_ENDING_PATH "%s/Oneshot/ending", getenv("APPDATA")
+
+#define ONESHOT_DOCUMENT_PATH "%s/My Documents/DOCUMENT.oneshot.txt", getenv("HOMEPATH")
+#else
+#define ONESHOT_PRESAVE_COMMAND system("mkdir -p ~/.config/Oneshot");
+#define ONESHOT_WIPESAVE_COMMAND unlink(buf);
+#define ONESHOT_SAVE_PATH "%s/.config/Oneshot/save", getenv("HOME")
+#define ONESHOT_ENDING_PATH "%s/.config/Oneshot/ending", getenv("HOME")
+
+#define ONESHOT_DOCUMENT_PATH "%s/DOCUMENT.oneshot.txt", getenv("HOME")
+#endif
+
+// Use SDL for messageboxes. (NYI)
+// #define ONESHOT_SDL_MESSAGEBOXES
+
+// -- The following options affect gameplay to handle lacking parts of platform support (oneshot_ser).
+
+// This option will replace the Game Browser title with the safe code when the document is supposed to appear.
+// The following define (ENTITY_AWARE_OF_EMULATION) assumes you have engaged this,
+//  hence it asks that the player sleep to allow them to exit the 
+#define ONESHOT_GAME_BROWSER_SHOWS_SAFE_CODE
+
+// This option will replace STR_STILL_HAVING_TROUBLE with an altered message that hints to the location
+//  of the new safe code in the Game Browser. This is as best as I can do without altering game event-code.
+// #define ONESHOT_ENTITY_AWARE_OF_EMULATION
+
+// This option will replace STR_DO_YOU_UNDERSTAND with an absolute spoiler to the safe code,
+//  without having to sleep in order to see it in the Game Browser.
+// (SOME EDITS LATER: Basically necessary because the Entity
+//   refuses to let Niko sleep until a progression point.
+//  Fun for debugging. Not.)
+//#define ONESHOT_ABSOLUTELY_CHEAT_SINCE_WE_CANT_META
+
+// ----
+
 #include <string>
 
 #define ONESHOT_VAR_FUNC 1
--- src/oneshot_ser.cpp
+++ src/oneshot_ser.cpp
@@ -29,6 +29,7 @@
 #include "oneshot_ser.h"
 #include "output.h"
 
+#ifndef ONESHOT_DATA_PERSISTENCE
 static int oneshot_ser_ending = 0;
 
 static unsigned char oneshot_ser_hasSave = 0;
@@ -40,17 +41,73 @@ static unsigned char * oneshot_ser_iptr;
 
 static uint8_t oneshot_ser_switches[200];
 static int32_t oneshot_ser_vars[100];
+#else
+
+#include <stdio.h>
+
+static FILE * oneshot_ser_workingfile = 0;
+
+#define ONESHOT_SER_ALERTFILEFAIL FILE * a = fopen(buf, mode); if (!a) Output::Debug("Couldn't open %s mode %s", buf, mode); return a;
+#define ONESHOT_SER_SEQS char buf[2048]; snprintf(buf, 2048,
+#define ONESHOT_SER_SEQE ); ONESHOT_SER_ALERTFILEFAIL
+#define ONESHOT_SER_SEQET );
+
+static FILE * open_ending_with_mode(char * mode) {
+	ONESHOT_PRESAVE_COMMAND
+	ONESHOT_SER_SEQS ONESHOT_ENDING_PATH ONESHOT_SER_SEQE
+}
+static FILE * open_save_with_mode(char * mode) {
+	ONESHOT_PRESAVE_COMMAND
+	ONESHOT_SER_SEQS ONESHOT_SAVE_PATH ONESHOT_SER_SEQE
+}
+static FILE * open_document_with_mode(char * mode) {
+	ONESHOT_SER_SEQS ONESHOT_DOCUMENT_PATH ONESHOT_SER_SEQE
+}
+static void wipe_save() {
+	ONESHOT_SER_SEQS ONESHOT_SAVE_PATH ONESHOT_SER_SEQET
+	ONESHOT_WIPESAVE_COMMAND
+}
+#undef ONESHOT_SER_ALERTFILEFAIL
+#undef ONESHOT_SER_SEQS
+#undef ONESHOT_SER_SEQE
+#undef ONESHOT_SER_SEQET
+
+#endif
 
 int oneshot_ser_loadEnding() {
+#ifndef ONESHOT_DATA_PERSISTENCE
 	return oneshot_ser_ending;
+#else
+	FILE * f = open_ending_with_mode("rb");
+	if (f) {
+		int i = fgetc(f);
+		if (i < 0)
+			i = 0;
+		fclose(f);
+		return i;
+	}
+	return 0;
+#endif
 }
 
 void oneshot_ser_saveEnding(int ending) {
+#ifndef ONESHOT_DATA_PERSISTENCE
 	oneshot_ser_ending = ending;
+#else
+	FILE * f = open_ending_with_mode("wb");
+	if (f) {
+		fputc(ending, f);
+		fclose(f);
+	}
+#endif
 }
 
 void oneshot_ser_wipeSave() {
+#ifndef ONESHOT_DATA_PERSISTENCE
 	oneshot_ser_hasSave = 0;
+#else
+	wipe_save();
+#endif
 }
 
 // Header Format: 1 byte for the size of the username.
@@ -58,6 +115,7 @@ void oneshot_ser_wipeSave() {
 // 200 bytes of switches.
 // 100 variables, each 4 bytes, little-endian.
 void oneshot_ser_saveBegin(uint8_t * switches, int32_t * vars, char * username, uint32_t usernameSize) {
+#ifndef ONESHOT_DATA_PERSISTENCE
 	oneshot_ser_usernameSize = usernameSize;
 	memcpy(oneshot_ser_username, username, usernameSize);
 
@@ -65,23 +123,49 @@ void oneshot_ser_saveBegin(uint8_t * switches, int32_t * vars, char * username, 
 
 	memcpy(oneshot_ser_switches, switches, 200);
 	memcpy(oneshot_ser_vars, vars, 100 * sizeof(int32_t));
+#else
+	oneshot_ser_workingfile = open_save_with_mode("wb");
+	if (oneshot_ser_workingfile) {
+		fputc(usernameSize, oneshot_ser_workingfile);
+		fwrite(username, usernameSize, 1, oneshot_ser_workingfile);
+		fwrite(switches, 200, 1, oneshot_ser_workingfile);
+		for (int i = 0; i < 100; i++) {
+			// For portability.
+			fputc(vars[i] & 0xFF, oneshot_ser_workingfile);
+			fputc((vars[i] >> 8) & 0xFF, oneshot_ser_workingfile);
+			fputc((vars[i] >> 16) & 0xFF, oneshot_ser_workingfile);
+			fputc((vars[i] >> 24) & 0xFF, oneshot_ser_workingfile);
+		}
+	}
+#endif
 }
 // Item format is essentially a string - it's zero-terminated.
 // Writing is controlled mostly by the game script.
 // When a zero is written, the file is closed.
 // (Note: there are no safety checks on any of this)
 void oneshot_ser_saveItem(int item) {
+#ifndef ONESHOT_DATA_PERSISTENCE
 	*(oneshot_ser_iptr++) = item;
 	if (item == 0)
 		oneshot_ser_hasSave = 1;
+#else
+	if (oneshot_ser_workingfile) {
+		fputc(item, oneshot_ser_workingfile);
+		if (item == 0) {
+			fclose(oneshot_ser_workingfile);
+			oneshot_ser_workingfile = 0;
+		}
+	}
+#endif
 }
 
 // 0: No save file exists.
-// 1: A save file exists. It has a player name.
-// 2: A save file exists. It has no player name.
+// 1: A save file exists.
+// (The 1/2 disambiguation has been moved to oneshot.cpp to make life easier here)
 // The calling function is responsible for updating the "oneshot" and "ending" internal variables,
 //  along with the cached on-quit message.
 int oneshot_ser_loadBegin(uint8_t * switches, int32_t * vars, char * username, uint32_t * usernameSize) {
+#ifndef ONESHOT_DATA_PERSISTENCE
 	if (!oneshot_ser_hasSave)
 		return 0;
 
@@ -94,14 +178,56 @@ int oneshot_ser_loadBegin(uint8_t * switches, int32_t * vars, char * username, u
 	memcpy(switches, oneshot_ser_switches, 200);
 	memcpy(vars, oneshot_ser_vars, 100 * sizeof(int32_t));
 	return 1;
+#else
+	oneshot_ser_workingfile = open_save_with_mode("rb");
+	if (oneshot_ser_workingfile) {
+		int us = fgetc(oneshot_ser_workingfile);
+		if (us < 0) {
+			// file is clearly corrupt / has been blanked out by a bad save-wipe routine
+			fclose(oneshot_ser_workingfile);
+			return 0;
+		}
+		*usernameSize = us;
+		if (*usernameSize) {
+			// just hope the file is valid from this point on *gulp*
+			fread(username, us, 1, oneshot_ser_workingfile);
+			username[us] = 0;
+			return 1;
+		}
+	} else {
+		return 0;
+	}
+#endif
 }
 int oneshot_ser_loadItem() {
+#ifndef ONESHOT_DATA_PERSISTENCE
 	return *(oneshot_ser_iptr++);
+#else
+	if (oneshot_ser_workingfile) {
+		int i = fgetc(oneshot_ser_workingfile);
+		if (i < 1) {
+			fclose(oneshot_ser_workingfile);
+			oneshot_ser_workingfile = 0;
+			return 0;
+		}
+		return i;
+	} else {
+		return 0;
+	}
+#endif
 }
 
-void oneshot_ser_document(char * string) {
+void oneshot_ser_document(char * str) {
+#ifndef ONESHOT_DATA_PERSISTENCE
 	// For debugging
-	Output::Debug("Document: ||%s||", string);
+	Output::Debug("Document: ||%s||", str);
+#else
+	FILE * doc = open_document_with_mode("wb");
+	if (doc) {
+		fputs(str, doc);
+		fclose(doc);
+	}
+#endif
 }
 
 void oneshot_ser_setwp(bool enabled) {
@@ -120,4 +246,10 @@ void oneshot_ser_shakewindow() {
 void oneshot_ser_leavewindow() {
 	// For debugging
 	Output::Debug("Niko walks off the screen...");
+}
+
+bool oneshot_ser_trymsgbox(const char * text, const char * title, int msg_type, void (*cb)(void * a, bool result), void * userdata) {
+	// This basic backend is enough to run the game, but meta stuff doesn't work here.
+	// (let's just hope you remember that wallpaper 'kay)
+	return false;
 }
