1e0058a6309e7991afef810f121b4f427ec5676f
20kdc <asdd2808@gmail.com>
2017-02-09 18:28:08 +0000

    [ONESHOT-SPECIFIC] Message box stuff, and fixes involving the ENDING_DEAD sequence
    Hopefully it should work like the original game now?
    Or at least to whatever extent I can actually make that happen, anyway.
    Alert me if anything's wrong.

--- src/scene_osmb.cpp
+++ src/scene_osmb.cpp
@@ -30,24 +30,32 @@
 #include "util_macro.h"
 #include "bitmap.h"
 #include "oneshot.h"
+#include "oneshot_ser.h"
 #include "output.h"
 
-Scene_OSMB::Scene_OSMB(const char * text, bool yn, std::shared_ptr<Scene> const& next_scene) {
+Scene_OSMB::Scene_OSMB(const char * text, const char * title, int yn, std::shared_ptr<Scene> const& next_scene) {
 	Scene::type = Scene::End;
 	str = text;
+	str2 = title;
 	yesNo = yn;
+	tryPtr = 0;
 	nextBox = next_scene;
 	hasInputControl = false;
 	creationMBC = ++oneshot_global_messagebox_count;
-	answered = false;
+	answered = false;	
 }
 
 void Scene_OSMB::Start() {
-	CreateCommandWindow();
-	CreateHelpWindow();
-	FileRequestAsync* request = AsyncHandler::RequestFile("System", "pc");
-	request_id = request->Bind(&Scene_OSMB::OnWindowskinReady, this);
-	request->Start();
+	// The first one to be Start()'d is always the first message box.
+	tryPtr = oneshot_ser_trymsgbox(str.c_str(), str2.c_str(), yesNo);
+	if (!tryPtr) {
+		// Notice this only happens if trymsgbox fails!
+		CreateCommandWindow();
+		CreateHelpWindow();
+		FileRequestAsync* request = AsyncHandler::RequestFile("System", "pc");
+		request_id = request->Bind(&Scene_OSMB::OnWindowskinReady, this);
+		request->Start();
+	}
 }
 
 void Scene_OSMB::OnWindowskinReady(FileRequestResult* result) {
@@ -55,11 +63,38 @@ void Scene_OSMB::OnWindowskinReady(FileRequestResult* result) {
 	command_window->ForceWindowskin(result->file);
 }
 
+void Scene_OSMB::FinishMsgbox() {
+	Game_Map::SetNeedRefresh(Game_Map::Refresh_All);
+	oneshot_global_messagebox_count--;
+	answered = true;
+	if (nextBox) {
+		// This may have the side effect of destroying this object.
+		// (The worrying thing is that I'm not sure *when*.)
+		Scene::Push(nextBox, true);
+		return;
+	} else {
+		if (oneshot_global_messagebox_count)
+			Output::Warning("Messagebox-count inconsistent with scene linked list.");
+		Scene::Pop();
+	}
+}
+
 void Scene_OSMB::Update() {
-	command_window->Update();
+	if (tryPtr) {
+		// Don't do any of that at all
+		int r = oneshot_ser_msgbox_update(tryPtr);
+		if (r != ONESHOT_SER_MSGBOX_UNFINISHED) {
+			Game_Variables[ONESHOT_VAR_RETURN] = r == ONESHOT_SER_MSGBOX_YES ? 1 : 0;
+			tryPtr = 0;
+			FinishMsgbox();
+		}
+		return;
+	}
 
 	if (answered)
 		return;
+
+	command_window->Update();
 
 	bool topStack = oneshot_global_messagebox_count == creationMBC;
 	if (!topStack)
@@ -79,19 +114,7 @@ void Scene_OSMB::Update() {
 		case 1: // No
 			Game_Variables[ONESHOT_VAR_RETURN] = 0;
 		}
-		Game_Map::SetNeedRefresh(Game_Map::Refresh_All);
-		oneshot_global_messagebox_count--;
-		answered = true;
-		if (nextBox) {
-			// This may have the side effect of destroying this object.
-			// (The worrying thing is that I'm not sure *when*.)
-			Scene::Push(nextBox, true);
-			return;
-		} else {
-			if (oneshot_global_messagebox_count)
-				Output::Warning("Messagebox-count inconsistent with scene linked list.");
-			Scene::Pop();
-		}
+		FinishMsgbox();
 	}
 }
 
@@ -99,7 +122,7 @@ void Scene_OSMB::CreateCommandWindow() {
 	// Create Options Window
 	std::vector<std::string> options;
 	options.push_back(Data::terms.yes);
-	if (yesNo)
+	if (yesNo == MESSAGE_QUESTION)
 		options.push_back(Data::terms.no);
 
 	command_window.reset(new Window_Command(options));
